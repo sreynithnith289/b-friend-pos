6906ad5a37602ec19829ea6b6315ac6fd915eb22
sreynithnith289 <sreynithnith289@gmail.com>
2026-02-05 13:23:13 +0700

    Add redux-persist to fix login redirect

--- pos-frontend/src/main.jsx
+++ pos-frontend/src/main.jsx
@@ -1,9 +1,39 @@
+// import { StrictMode } from "react";
+// import { createRoot } from "react-dom/client";
+// import "./index.css";
+// import App from "./App.jsx";
+// import { Provider } from "react-redux";
+// import store from "./redux/store.js";
+// import { SnackbarProvider } from "notistack";
+// import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+// const queryClient = new QueryClient({
+//   defaultOptions: {
+//     queries: {
+//       staleTime: 30000,
+//     },
+//   },
+// });
+
+// createRoot(document.getElementById("root")).render(
+//   <StrictMode>
+//     <Provider store={store}>
+//       <SnackbarProvider autoHideDuration={3000}>
+//         <QueryClientProvider client={queryClient}>
+//           <App />
+//         </QueryClientProvider>
+//       </SnackbarProvider>
+//     </Provider>
+//   </StrictMode>
+// );
+
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import { Provider } from "react-redux";
-import store from "./redux/store.js";
+import store, { persistor } from "./redux/store.js";
+import { PersistGate } from "redux-persist/integration/react";
 import { SnackbarProvider } from "notistack";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
@@ -18,11 +48,13 @@ const queryClient = new QueryClient({
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Provider store={store}>
-      <SnackbarProvider autoHideDuration={3000}>
-        <QueryClientProvider client={queryClient}>
-          <App />
-        </QueryClientProvider>
-      </SnackbarProvider>
+      <PersistGate loading={null} persistor={persistor}>
+        <SnackbarProvider autoHideDuration={3000}>
+          <QueryClientProvider client={queryClient}>
+            <App />
+          </QueryClientProvider>
+        </SnackbarProvider>
+      </PersistGate>
     </Provider>
   </StrictMode>
 );
--- pos-frontend/src/redux/store.js
+++ pos-frontend/src/redux/store.js
@@ -1,15 +1,54 @@
-import { configureStore } from "@reduxjs/toolkit";
+// import { configureStore } from "@reduxjs/toolkit";
+// import customerSlice from "./slices/customerSlice";
+// import cartSlice from "./slices/cartSlice";
+// import userSlice from "./slices/userSlice";
+
+// const store = configureStore({
+//   reducer: {
+//     customer: customerSlice,
+//     cart: cartSlice,
+//     user: userSlice,
+//   },
+//   devTools: import.meta.env.NODE_ENV !== "production",
+// });
+
+// export default store;
+
+
+
+
+import { configureStore, combineReducers } from "@reduxjs/toolkit";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+
 import customerSlice from "./slices/customerSlice";
 import cartSlice from "./slices/cartSlice";
 import userSlice from "./slices/userSlice";
 
-const store = configureStore({
-  reducer: {
-    customer: customerSlice,
-    cart: cartSlice,
-    user: userSlice,
-  },
-  devTools: import.meta.env.NODE_ENV !== "production",
+const persistConfig = {
+  key: "bfriend-pos",
+  storage,
+  whitelist: ["user", "cart"], // persist user login and cart
+};
+
+const rootReducer = combineReducers({
+  customer: customerSlice,
+  cart: cartSlice,
+  user: userSlice,
 });
 
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+const store = configureStore({
+  reducer: persistedReducer,
+  devTools: import.meta.env.NODE_ENV !== "production",
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: ["persist/PERSIST", "persist/REHYDRATE"],
+      },
+    }),
+});
+
+export const persistor = persistStore(store);
 export default store;
